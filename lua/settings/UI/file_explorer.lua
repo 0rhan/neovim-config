0ffd04be42a69aaf1844fff14860e3604fd1a7ff
0rhan <gurbanov.dev@gmail.com>
2022-08-05 10:59:22 +0300

    Update nvim_tree config

--- lua/settings/UI/file_explorer.lua
+++ lua/settings/UI/file_explorer.lua
@@ -1,41 +1,6 @@
 local g = vim.g
 local highlight = vim.highlight
 
-g.nvim_tree_git_hl = 1
-g.nvim_nvim_tree_highlight_opened_files = 1
-g.nvim_tree_add_trailing = 1
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-  folder_arrows =1
-}
-
-g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-     unstaged = "✗",
-     staged = "✓",
-     unmerged = "",
-     renamed = "➜",
-     untracked = "★",
-     deleted = "",
-     ignored = "◌"
-  },
-  folder = {
-    arrow_open = "",
-    arrow_closed = "",
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-    symlink_open = "",
-  },
-}
-
-
 require'nvim-tree'.setup {
   disable_netrw        = false,
   hijack_netrw         = true,
@@ -52,10 +17,23 @@ require'nvim-tree'.setup {
     auto_open = true,
   },
   renderer = {
+    highlight_git = true,
+    highlight_opened_files = "all",
+    add_trailing = true,
     indent_markers = {
       enable = true,
-    };
-  };
+    },
+  icons = {
+    webdev_colors = true,
+    git_placement = "before",
+    show = {
+     file = true,
+     folder = true,
+     folder_arrow = true,
+     git = true,
+    },
+  },
+  },
   diagnostics = {
     enable = enable,
     icons = {
