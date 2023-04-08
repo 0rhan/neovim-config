df602bf0fa9d6a6e5e44373fb188620b56a49faf
0rhan <gurbanov.code@gmail.com>
2023-04-08 19:21:54 +0300

    Change diagnostics appearance

--- lua/settings/UI/interface.lua
+++ lua/settings/UI/interface.lua
@@ -2,13 +2,51 @@ local globalOptions = vim.o
 local windowOptions = vim.wo
 local bufferOptions = vim.bo
 
-globalOptions.background = 'dark'
+globalOptions.background = "dark"
 globalOptions.termguicolors = true
 windowOptions.cursorline = true
 windowOptions.number = true
 windowOptions.list = true
-globalOptions.listchars = 'eol:↴,tab:,trail:·,space:·,'
+globalOptions.listchars = "eol:↴,tab:,trail:·,space:·,"
+vim.o.updatetime = 250
 
+local signs = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " ",
+}
+
+-- [DIAGNOSTICS]
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●", -- Could be '●', '▎', 'x'
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = false,
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = bufnr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "always",
+      prefix = " ",
+      scope = "cursor",
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end,
+})
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 -- [THEME CONFIG] --
 vim.g.tokyonight_style = "night"
@@ -16,4 +54,4 @@ vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd([[colorscheme tokyonight]])
