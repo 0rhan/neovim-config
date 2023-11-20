5c3fbb99be1ca61c9d6a78bcdab385214315c7b9
0rhan <gurbanov.code@gmail.com>
2023-11-20 23:59:17 +0300

    Disable netrw

--- lua/settings/editor.lua
+++ lua/settings/editor.lua
@@ -4,6 +4,9 @@ local bufferOptions = vim.bo
 
 globalOptions.hidden = true
 globalOptions.mouse = 'a'
+-- disable netrw
+globalOptions.loaded_netrw = 1
+globalOptions.loaded_netrwPlugin = 1
 
 bufferOptions.syntax='on'
 
